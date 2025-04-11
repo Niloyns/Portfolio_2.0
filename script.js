10357b5535f646aa7f15e01ad693f34aaf81a630
Niloyns <niloysarkar1998@gmail.com>
2025-04-12 00:54:57 +0530

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+let skill = document.getElementById(element);
+
+skill = new Typed("#element", {
+  strings: [
+    "Software Engineer",
+    "Front End Developer",
+    "Back End Developer",
+    "MERN Stack Developer",
+    "Full Stack Developer",
+  ],
+  typeSpeed: 90,
+  loop: true,
+});
