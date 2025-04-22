e6554ef247f3956375d315f0a401632285fcfa98
Niloyns <niloysarkar1998@gmail.com>
2025-04-22 16:47:21 +0530

    add animation

--- script.js
+++ script.js
@@ -23,25 +23,31 @@ gsap
     },
     0
   )
+
   .from(
     "nav",
     {
-      y: -100, // Slide nav in from top
+      y: -50, // Slide nav in from top
       duration: 2,
+      opacity: -2,
       ease: "power2.out",
     },
     0
   )
+
   .from(
     "nav ul li",
     {
-      y: -100, // Slide nav in from top
+      y: -100, // Slide nav items in from top
       duration: 2,
+      opacity: -5,
+      delay: 0.5,
       ease: "power2.out",
       stagger: 0.3,
     },
     0
   )
+
   .from(
     ".name",
     {
@@ -53,6 +59,7 @@ gsap
     },
     0
   )
+
   .from(
     ".titel",
     {
@@ -65,6 +72,7 @@ gsap
     },
     0
   )
+
   .from(
     ".typed_skill",
     {
@@ -75,6 +83,7 @@ gsap
     },
     0
   )
+
   .from(
     ".sumary",
     {
@@ -86,6 +95,7 @@ gsap
     },
     0
   )
+
   .from(
     ".social_icon i",
     {
@@ -94,7 +104,59 @@ gsap
       delay: 1,
       duration: 2, // Reduced duration for each icon
       ease: "power2.out",
-      stagger: 0.3, // Stagger the animation by 0.2 seconds
+      stagger: 0.3, // Stagger the animation by 0.3 seconds
     },
     0
   );
+
+// Scroll-triggered animation for .about-section h3
+gsap.from(".about-section h3", {
+  scale: 0,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".about-section h3",
+    scroller: "body",
+    start: "top 95%",
+    scrub: 1,
+  },
+});
+
+// .about animation
+gsap.from(".about", {
+  x: -200,
+  opacity: 0,
+  duration: 1.5,
+  scrollTrigger: {
+    trigger: ".about",
+    start: "top 80%",
+    end: "top 35%",
+    scrub: 2,
+    markers: true,
+  },
+});
+
+// .edu-container animation
+gsap.from(".edu-container", {
+  x: 200,
+  opacity: 0,
+  duration: 1.5,
+  scrollTrigger: {
+    trigger: ".edu-container",
+    start: "top 80%",
+    end: "top 35%",
+    scrub: 2,
+    markers: true,
+  },
+});
+
+gsap.from(".skillSection h3", {
+  scale: 0,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".skillSection h3",
+    start: "top 90%",
+    end: "top 35",
+    scrub: 2,
+  },
+});
